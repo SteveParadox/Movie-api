a5588bfeeaa07b28a0db5756649c8fbf442cc1fb
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-10-06 23:36:06 +0100

    Update config.py

--- Api/config.py
+++ Api/config.py
@@ -5,7 +5,7 @@ class Config:
     def __init__(self):
         pass
 
-    ENV = 'dev'
+    ENV = 'prod'
 
     if ENV == 'dev':
         SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -14,6 +14,6 @@ class Config:
     else:
         SECRET_KEY = "795849f0d2328258710ae9c71cb4b5ea"
         API_KEY = "795849f0d2328258710ae9c71cb4b5ea"
-        SQLALCHEMY_DATABASE_URI = ''
+        SQLALCHEMY_DATABASE_URI = 'postgres://ufiptzttzuntox:387caedfd883109ecc8be4445c7f6b8d24ade865c271a07d804ccba4d92a5be6@ec2-54-160-161-214.compute-1.amazonaws.com:5432/d4avvp9u9sf1n5'
 
 
