59797f8e7cd38aeea8a17e6d620cd5244154eb9b
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-10-30 15:26:34 +0100

    Add files via upload

--- Api/__init__.py
+++ Api/__init__.py
@@ -5,19 +5,21 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from .config import Config
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS()
 app.config.from_object(Config)
 db = SQLAlchemy()
 ma = Marshmallow()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'users.login'
+login_manager.login_view = 'users.login_users'
 login_manager.login_message = None
 login_manager.session_protection = "strong"
 REMEMBER_COOKIE_NAME= 'remember_token'
 REMEMBER_COOKIE_DURATION=datetime.timedelta(days=64, seconds=29156, microseconds=10)
-REMEMBER_COOKIE_REFRESH_EACH_REQUEST=True
+REMEMBER_COOKIE_REFRESH_EACH_REQUEST=False
 
 
 def create_app(config_class=Config):
@@ -27,11 +29,16 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     ma.init_app(app)
     login_manager.init_app(app)
+    cors.init_app(app)
    # mail.init_app(app)
     # socketio.init_app(app)
     #jwt.init_app(app)
-    from .routes import api
+    from .api.routes import api
+    from .users.routes import users
+    from .upload.routes import upload
 
     app.register_blueprint(api)
+    app.register_blueprint(users)
+    app.register_blueprint(upload)
 
     return app
--- Api/config.py
+++ Api/config.py
@@ -5,15 +5,16 @@ class Config:
     def __init__(self):
         pass
 
-    ENV = 'prod'
+    ENV = 'dev'
 
     if ENV == 'dev':
-        SECRET_KEY = os.environ.get('SECRET_KEY')
+        SECRET_KEY = 'os.environ.ge'
         API_KEY = os.environ.get('SECRET_KEY')
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+        CORS_HEADERS = 'Content-Type'
     else:
         SECRET_KEY = "795849f0d2328258710ae9c71cb4b5ea"
         API_KEY = "795849f0d2328258710ae9c71cb4b5ea"
         SQLALCHEMY_DATABASE_URI = 'postgres://ufiptzttzuntox:387caedfd883109ecc8be4445c7f6b8d24ade865c271a07d804ccba4d92a5be6@ec2-54-160-161-214.compute-1.amazonaws.com:5432/d4avvp9u9sf1n5'
-
+        CORS_HEADERS = 'Content-Type'
 
