9c8b69521d898107965bbf3cfd7bc78440779247
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-11-05 16:37:47 +0100

    Add files via upload

--- Api/models.py
+++ Api/models.py
@@ -7,10 +7,10 @@ from marshmallow_sqlalchemy import ModelSchema
 from . import db, login_manager
 
 
-
 @login_manager.user_loader
 def load_user(users_id):
     return Users.query.get(int(users_id))
+
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,24 +24,48 @@ class Movie(db.Model):
     poster = db.Column(db.String)
     poster_data = db.Column(db.LargeBinary)
     date_uploaded = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    
 
-    
+
 class Users(db.Model, UserMixin):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String())
-  email = db.Column(db.String())
-  dob = db.Column(db.String())
-  password = db.Column(db.String())
-  vid_time= db.Column(db.String())
-  pair= db.Column(db.String())
-  
-  
-  def __repr__(self):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    dob = db.Column(db.String())
+    password = db.Column(db.String())
+    pair = db.Column(db.String())
+    friends = db.relationship('Friend', backref='get', lazy=True)
+
+    def __repr__(self):
         return f"User('{self.name}', '{self.email}')"
-  
-      
-  
+
+
+class Friend(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    u_friend = db.Column(db.String())
+
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    unique_id = db.Column(db.String(), unique=True)
+    host = db.Column(db.String())
+    vid_time = db.Column(db.String())
+    admin= db.Column(db.Boolean, default=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __repr__(self):
+        return f"Room('{self.unique_id}', '{self.host}', '{self.admin}')"
+
+
+
+class FriendSchema(ModelSchema):
+    class Meta:
+        model = Friend
+
+
+friend_schema = FriendSchema
+friends_schema = FriendSchema(many=True)
+
 class MovieSchema(ModelSchema):
     class Meta:
         model = Movie
@@ -49,6 +73,15 @@ class MovieSchema(ModelSchema):
 
 movie_schema = MovieSchema
 movies_schema = MovieSchema(many=True)
+
+
+class RoomSchema(ModelSchema):
+    class Meta:
+        model = Room
+
+
+room_schema = RoomSchema
+rooms_schema = RoomSchema(many=True)
 
 
 
