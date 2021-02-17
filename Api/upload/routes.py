0f97dcbf6f15c99173e29644516b7867a48455a7
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-02-17 09:15:18 +0100

    Add files via upload

--- Api/upload/routes.py
+++ Api/upload/routes.py
@@ -121,14 +121,14 @@ def upload_movie():
         movies.cast2 = lists[1]
         movies.cast3 = lists[2]
         movies.cast4 = lists[3]
-        genre = {}
+        genre = []
         company = dict_movie['production_companies']
         com = []
         for i in company:
             com.append(i['name'])
         for i in genres:
-            genre.update({"name": i['name']})
-        movies.genre = genre
+            genre.append(i['name'])
+        movies.genre = {"data": genre}
         movies.creator = com[0]
         movies.created_on = str(dict_movie['release_date'])
         movies.runtime = str(dict_movie['runtime'])
