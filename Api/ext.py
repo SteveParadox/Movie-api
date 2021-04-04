b4977935ba07771140469463b59d9451a81638a7
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2021-04-04 13:38:01 +0100

    Add files via upload

---
+++ Api/ext.py
@@ -0,0 +1,37 @@
+import random
+from flask import *
+from flask_login import current_user, login_required
+from Api import *
+from Api.models import Movie, MovieSchema, Data, DataSchema, Friend, Users, Exciting, Store, UserRating
+from flask_cors import cross_origin
+from functools import wraps
+import jwt
+
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        try:
+            incoming = request.get_json()
+            token = incoming['token']
+        except:
+            return jsonify({
+                'message' : 'No Token sent, User not logged in'
+            }), 401
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+        try:
+            data = jwt.decode(token, Config.SECRET_KEY)
+            current_user = Users.query\
+                .filter_by(email = data['email']).first()
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401 
+        
+        return  f(current_user, *args, **kwargs)
+   
+    return decorated
+
