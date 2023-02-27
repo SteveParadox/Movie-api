13a4856901f0bd9091c3a03fb0f79f9bafe0438c
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-02-27 04:34:19 +0100

    Update routes.py

--- Api/api/routes.py
+++ Api/api/routes.py
@@ -54,46 +54,44 @@ def loggedHome(current_user):
 
 
 
-@api.route('/api/genre', methods=['POST'])
+@api.route('/api/genres', methods=['POST'])
 @cross_origin()
-def genresGet():
-    data =request.get_json()
-    genreList = {}
-    dataLists = []
-    resultList = []
-    movies = Movie.query.all()
-    for i in data['genre']:
-        dataLists.append(i)
-    for j in movies:
-        genreList.update({"name":j.name, "genre": j.genre})
-    for x in dataLists:
-        if x in genreList['genre']:
-            resultList.append(genreList['name'])
+def get_movies_by_genre():
+    data = request.get_json()
+    genre_list = data['genres']
+    movies = Movie.query.filter(Movie.genre.in_(genre_list)).all()
+    result_list = []
+    for movie in movies:
+        result_list.append(movie.name)
     return jsonify({
-        "data": resultList
+        "movies": result_list
     }), 200
 
 
 
-# searching for movie
+
 @api.route('/api/search/movie', methods=['POST'])
 @cross_origin()
 def searchList():
     data = request.get_json()
-    movie_name = Movie.query.filter(name= str(data['name'][0]).upper()+data['name'][1:]).all()
-    if not movie_name:
-        movie_name = Movie.query.filter_by(genre=str(data['name'][0]).upper()+data['name'][1:]).all()
-        if not movie_name:
-            movie_name = Movie.query.filter_by(creator=str(data['name'][0]).upper()+data['name'][1:]).all()
-            if not movie_name:
-                return jsonify({
-                    "message": " Could not find Name"
-                }), 404
+    search_term = str(data['name'][0]).upper() + data['name'][1:]
+    movies = Movie.query.filter(
+        (Movie.name.ilike(f'%{search_term}%')) |
+        (Movie.genre.ilike(f'%{search_term}%')) |
+        (Movie.creator.ilike(f'%{search_term}%'))
+    ).all()
+
+    if not movies:
+        return jsonify({
+            "message": "Could not find Name"
+        }), 404
+
     movie_schema = MovieSchema(many=True)
-    result = movie_schema.dump(movie_name)
+    result = movie_schema.dump(movies)
     return jsonify({
         'data': result
     }), 200
+
 
 
 # link to redirect to selected movie
@@ -104,65 +102,48 @@ def searchList():
 def get_movie(current_user, u_id):
     try:
         movie_name = Movie.query.filter_by(public_id=u_id).first()
-        movie_name.popular = movie_name.popular + 1
+        movie_name.popular += 1
         db.session.commit()
         movie_schema = MovieSchema()
         result = movie_schema.dump(movie_name)
         id = current_user.id
         name = current_user.name
-        try:
-            return jsonify({
-                'data': result,
-                "user_id": id,
-                "user_name": name
-            }), 200
-        except:
-            return jsonify({
-                'message': "could not load data"
-            })
+        return jsonify({
+            'data': result,
+            "user_id": id,
+            "user_name": name
+        }), 200
+    except AttributeError:
+        return jsonify({
+            'message': "Could not find the movie"
+        }), 404
     except:
         return jsonify({
             'message': "Server error"
-        })
+        }), 500
 
 
 @api.route('/api/similar/movie/<string:u_id>', methods=['GET'])
 @cross_origin()
 def similar_movie(u_id):
-    similarList={}
-    genreList=[]
-    moviesSimilar=[]
     try:
-        movie_name = Movie.query.filter_by(public_id=u_id).first()
-        for i in movie_name.genre:
-            genreList.append(i)
-        movies = Movie.query.all()
-        for j in movies:
-            for k in j.genre:
-                if k in genreList:
-                    moviesSimilar.append(k)
-        movies_ = Movie.query.all()
-        for i,h in zip(movies_, moviesSimilar):
-            if h in i.genre:
-                similarList.update({
-                    "name": i.name,
-                    'id': i.public_id,
-                    "genre": i.genre,
-                    'overview': i.description
-        })
-        if u_id in similarList['id']:
-            similarList.pop('id')
-            similarList.pop('genre')
-            similarList.pop('name')
-            similarList.pop('overview')
-
+        movie = Movie.query.filter_by(public_id=u_id).first()
+        genres = movie.genre
+        movies = [m for m in Movie.query.all() if any(g in genres for g in m.genre)]
+        similarList = [{
+            "name": m.name,
+            "id": m.public_id,
+            "genre": m.genre,
+            "overview": m.description
+        } for m in movies if m.public_id != u_id]
         return jsonify({
-            'data': similarList
+            "data": similarList
         }), 200
     except:
         return jsonify({
-            'data': "Wrong Public Id Used"
-        }), 402
+            "message": "Could not find movie with the given public ID."
+        }), 404
+
 
 # thumbs up a movie
 @api.route('/api/like/movie/<string:u_id>', methods=['POST'])
@@ -222,35 +203,32 @@ def choicesUser(current_user):
 
 
 
-# using user's interested movies for data processing
 @api.route('/api/loved/movies', methods=['GET', 'POST', 'OPTIONS'])
 @cross_origin()
 @token_required
 def loved_movies(current_user):
     result = []
-    filter_data = []
     t = []
-    data = Exciting.query.filter_by(rate=current_user).all()
-    for i in data:
-        result.append(i.loved)
-    for c in result:
-        liked = Movie.query.filter_by(name=c).first()
-        movies = Movie.query.filter_by(genre=liked.genre).all()
+    
+    # Get all movies that the user has liked
+    liked_movies = Exciting.query.filter_by(rate=current_user).all()
+    for liked in liked_movies:
+        liked_movie = Movie.query.filter_by(name=liked.loved).first()
+        if liked_movie:
+            # Get all movies that have the same genre as the liked movie
+            movies = Movie.query.filter_by(genre=liked_movie.genre).all()
+            for movie in movies:
+                t.append({
+                    'name': movie.name,
+                    'genre': movie.genre,
+                    'id': movie.public_id,
+                    'overview': movie.description,
+                })
         
-        # get movies related to liked movies
-    movies= Movie.query.all()
-    for i,h in zip(selected_genres, movies):  
-        if str(i[0].upper())+i[1:] in h.genre:
-            t.append({'name': movie.name,
-                      'genre': movie.genre,
-                      'id': movie.public_id,
-                      'overview': movie.description,
-                      })
-        
-            
     return jsonify({
         'data': t
     })
+
 
 
 
@@ -273,29 +251,30 @@ def i_and_my_friend(name):
             _list = [i for i in result.items()]
             friend_list = [i for i in friend_result.items()]
             for mine, _friend in zip(_list, friend_list):
-                if mine[1] == True and _friend[1] == True:
+                if mine[1] and _friend[1]:
                     conjoin.append(mine[0])
             for genres in conjoin:
-                movie = Movie.query.filter_by(genre=f'{genres[0].upper() + genres[1:]}').all()
-
-                for i in movie:
+                movies = Movie.query.filter_by(genre=f'{genres[0].upper() + genres[1:]}').all()
+                for index, movie in enumerate(movies):
                     suggested_movies.append({
-                                            'name': i.name,
-                                             'id': i.public_id,
-                                             'genre': i.genre,
-                                             'runtime ': i.runtime,
-                                             "overview": i.description
-
-                                             })
-                    random.shuffle(suggested_movies)
-            # set(conjoin)
+                        'name': movie.name,
+                        'id': movie.public_id,
+                        'genre': movie.genre,
+                        'runtime ': movie.runtime,
+                        'overview': movie.description
+                    })
+            random.shuffle(suggested_movies)
             return jsonify({
-                "we both like ": suggested_movies
+                'we both like': suggested_movies
             })
+        else:
+            return jsonify({
+                'message': f'{name} is not your friend'
+            })
+    else:
         return jsonify({
-            "message": f"{user} is not your friend"
+            'message': f'{name} is not registered'
         })
-    return jsonify({"message": f"{name} not registered"})
 
 
 @api.route('/api/add/review/<string:movie_id>', methods=['POST'])
